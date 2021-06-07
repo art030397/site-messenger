02fdd2896eaf2443f30c112f3b12fdbeb386bc21
pavel <20305382@gmail.com>
2021-06-07 13:29:28 +0300

    Правки верстки

--- src/lib/callsMessages.js
+++ src/lib/callsMessages.js
@@ -1,23 +1,23 @@
 import './callsMessages.scss'
 
 export const callsMessages =
-    `<div class="hoverl_1a1 c-m-hover">
-        <div class="lbContainer_b81 m-h-inner">
-            <div class="labelButtons_2fc _bottom_f55 h-i-labels">
-                <div class="labelButton_dfb">
-                    <div class="text_ed4 callback_ecf label call">
-                        <div class="title">Перезвоним вам через 29 секунд!</div>
-                    </div>
-                </div>
-                <div class="labelButton_dfb">
-                    <div class="text_ed4 chat_b22 label msg">
-                        <div class="title">Оставить сообщение</div>
-                    </div>
-                </div>
-                <div class="buttonsCorner_03e bottomCorner_b65 triangle"></div>
-            </div>
-        </div>
-    </div>`;
+    `<modiv class="c-m-hover">
+        <modiv class="m-h-inner">
+            <modiv class="h-i-labels">
+                <modiv class="">
+                    <modiv class="label call">
+                        <modiv class="title">Перезвоним вам через 29 секунд!</modiv>
+                    </modiv>
+                </modiv>
+                <modiv class="">
+                    <modiv class="label msg">
+                        <modiv class="title">Оставить сообщение</modiv>
+                    </modiv>
+                </modiv>
+                <div class="triangle"></div>
+            </modiv>
+        </modiv>
+    </modiv>`;
 
 export const callsProcessor = () => {
 
